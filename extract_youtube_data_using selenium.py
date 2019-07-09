42e8fadb42b842d53d96ac1224b319ea1f54bae3
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-07-09 17:38:41 -0500

    updated extracting youtube data

--- extract_youtube_data_using selenium.py
+++ extract_youtube_data_using selenium.py
@@ -10,7 +10,7 @@ import pandas as pd
 import json
 import numpy as np
 from bs4 import BeautifulSoup
-
+import random
 from urllib.request import Request, urlopen
 from fake_useragent import UserAgent
 
@@ -26,10 +26,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common import exceptions
-from selenium.webdriver.support.ui import WebDriverWait 
 
 
-chrome = webdriver.Chrome('D:/emotionaldata/chromedriver')
+chrome = webdriver.Chrome('C:/ProgramData/chocolatey/bin/chromedriver.exe')
 
 def getProx():
     proxies_req = Request('https://www.sslproxies.org/')
@@ -76,42 +75,91 @@ def main():
     proxy = proxies[proxy_index] 
     
     global df
-    
-    for index, row in df.iterrows():
-        getProx()
-        proxy_index = random.randint(0, len(proxies) - 1)
-        proxy = proxies[proxy_index] 
-        req = Request('http://icanhazip.com')
-        req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
-        
-        alt_id = row["Altmetric_id"]
-        
-        mylist[]
-        alt_req = requests.get("https://api.altmetric.com/v1/id/" + str(alt_id))
-        alt_rs= json.loads(alt_req.text)
-        mylist.append({str(alt_id):alt_rs})
-        
-        y_ids = row['youtube_ids'].split()
-        for y_id in y_ids:
-            you_req = chrome.get("https://www.youtube.com/watch?v=" + y_id)
-            wait = WebDriverWait(chrome, 10)
-            v_title = wait.until(EC.presence_of_element_located(
-                           (By.CSS_SELECTOR,"h1.title yt-formatted-string"))).text
-            v_description =  wait.until(EC.presence_of_element_located(
-                                         (By.CSS_SELECTOR,"div#description yt-formatted-string"))).text
-            elements = chrome.find_elements_by_xpath(
-                    '//yt-formatted-string[@class = "style-scope ytd-toggle-button-renderer style-text"]')
-            stats=[]
-            for element in elements:
-                if "likes" in element.get_attribute("aria-label"):
-                    likes = int(element.get_attribute("aria-label").replace(",", "").split()[0])
-                    stats.append({'likeCount':likes})
-                if "dislikes" in element.get_attribute("aria-label"):
-                    dislikes = int(element.get_attribute("aria-label").replace(",", "").split()[0])
-                    stats.append({"dislikeCount":dislikes})
-            v_count = wait.until(EC.presence_of_element_located(
-                                         (By.CSS_SELECTOR,"div#count yt-view-count-renderer"))).text
-            count = int(v_count.replace(",", "").split()[0])
-            stats.append({"viewCount":count})
+
+    for index, row in list(df.iterrows())[7433:7435]:
+        try:
+            proxy_index = random.randint(0, len(proxies) - 1)
+            proxy = proxies[proxy_index] 
+            req = Request('http://icanhazip.com')
+            req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
             
+            alt_id = row["Altmetric_id"]
+            
+            mylist=[]
+            alt_req = requests.get("https://api.altmetric.com/v1/id/" + str(alt_id))
+            alt_rs= json.loads(alt_req.text)
+            print(alt_req.text)
+            mylist.append({str(alt_id):alt_rs})
+            
+            y_ids = row['youtube_ids'].split()
+            y_list = []
+            for y_id in y_ids:
+                y_data = {}
+                y_data['videoId'] = y_id
+                chrome.get("https://www.youtube.com/watch?v=" + y_id)
+                channel_id = chrome.find_element_by_xpath('//a[@class="yt-simple-endpoint style-scope yt-formatted-string"]')
+                c_id = channel_id.get_attribute('href')
+                y_data["channelId"] = c_id[32:]
+                print("ID")
+                v_title =chrome.find_element_by_xpath('//h1[@class="title style-scope ytd-video-primary-info-renderer"]')
+                y_data["title"] = v_title.text
+                print("title")
+                v_description =  chrome.find_element_by_xpath('//div[@id = "description" and @class="style-scope ytd-video-secondary-info-renderer"]')
+                y_data["description"] = v_description.text
+                print("description")
+                elements = chrome.find_elements_by_xpath(
+                        '//yt-formatted-string[@class = "style-scope ytd-toggle-button-renderer style-text"]')
+                for element in elements:
+                    if "likes" in element.get_attribute("aria-label"):
+                        likes = int(element.get_attribute("aria-label").replace(",", "").split()[0])
+                        y_data['likeCount'] = likes
+                    if "dislikes" in element.get_attribute("aria-label"):
+                        dislikes = int(element.get_attribute("aria-label").replace(",", "").split()[0])
+                        y_data["dislikeCount"] = dislikes
+                print("likes and dislikes")
+                v_count = chrome.find_element_by_xpath('//div[@id = "count" and @class="style-scope ytd-video-primary-info-renderer"]')
+                count = int(v_count.text.replace(",", "").split()[0])
+                y_data["viewCount"] = count
+                print("viewCount")
+                n_comments = chrome.find_element_by_xpath('//h2[@id="count" and @class="style-scope ytd-comments-header-renderer"]')
+                n_comm = int(n_comments.text.replace(",", "").split()[0])
+                y_data["commentCount"] = n_comm
+                print("commentCount")
+                y_list.append(y_data)
+            mylist.append({"youtube":y_list})
+            file = open(str(alt_id) + ".txt", "w+")
+            json.dump(mylist, file)
+            file.close()
+        except NoSuchElementException:
+            print('NoSuch')
+            alt_id = row["Altmetric_id"]
+            file = open(str(alt_id) + ".txt", "w+")
+            file.close()
+            pass
+            
+        except exceptions.StaleElementReferenceException:
+            print('Stale')
+            alt_id = row["Altmetric_id"]
+            file = open(str(alt_id) + ".txt", "w+")
+            file.close()
+            pass
         
+        if index % 5 == 0:
+            getProx()
+            proxy_index = random_proxy()
+            proxy = proxies[proxy_index]
+            
+    
+        # Make the call
+        try:
+            my_ip = urlopen(req).read().decode('utf8')        
+            print('#' + str(n) + ': ' + my_ip)
+                
+          
+        except:  
+            # If error, delete this proxy and find another one
+            #global proxies
+            del proxies[proxy_index]
+            print('proxy deleted')
+            proxy_index = random_proxy()
+            proxy = proxies[proxy_index]
