371d1e3008d4b1854c39013b369c85f4c0fa1617
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-07-10 18:55:47 -0500

    added another script for extracting youtube data

--- extract_youtube_data_using selenium.py
+++ extract_youtube_data_using selenium.py
@@ -14,6 +14,8 @@ import random
 from urllib.request import Request, urlopen
 from fake_useragent import UserAgent
 
+from time import sleep
+
 df = pd.read_csv("C://Users//srika//Youtube-Videos//youtube.csv")
 df.columns = ["Altmetric_id", "youtube_ids"]
 
@@ -76,13 +78,8 @@ def main():
     
     global df
 
-    for index, row in list(df.iterrows())[7438:7442]:
+    for index, row in list(df.iterrows())[7447:7449]:
         try:
-            proxy_index = random.randint(0, len(proxies) - 1)
-            proxy = proxies[proxy_index] 
-            req = Request('http://icanhazip.com')
-            req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
-            
             alt_id = row["Altmetric_id"]
             
             mylist=[]
@@ -93,12 +90,17 @@ def main():
             
             y_ids = row['youtube_ids'].split()
             y_list = []
-            for y_id in y_ids:
+            for i in range(len(y_ids)):
                 y_data = {}
-                y_data['videoId'] = y_id
-                chrome.get("https://www.youtube.com/watch?v=" + y_id)
-                chrome.implicitly_wait(20)
-                chrome.execute_script("window.scrollTo(0, 5000);")
+                y_data['videoId'] = y_ids[i]
+                proxy_index = random.randint(0, len(proxies) - 1)
+                proxy = proxies[proxy_index] 
+                req = Request('http://icanhazip.com')
+                req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
+                sleep(0.5)
+                chrome.get("https://www.youtube.com/watch?v=" + y_ids[i])
+                chrome.implicitly_wait(30)
+                chrome.execute_script("window.scrollTo(0, 3000);")
                 channel_id = chrome.find_element_by_xpath('//a[@class="yt-simple-endpoint style-scope yt-formatted-string"]')
                 c_id = channel_id.get_attribute('href')
                 y_data["channelId"] = c_id[32:]
@@ -109,20 +111,20 @@ def main():
                 elements = chrome.find_elements_by_xpath(
                     '//yt-formatted-string[@id= "text" and @class = "style-scope ytd-toggle-button-renderer style-text"]')
                 for element in elements:
-                    if "dislikes" in element.get_attribute("aria-label") or "dislike" in element.get_attribute("aria-label"):
-                        try:
+                    try:
+                        if "dislikes" in element.get_attribute("aria-label") or "dislike" in element.get_attribute("aria-label"):
                             dislikes = int (element.get_attribute("aria-label").replace(",", "").split()[0])
                             y_data["dislikeCount"] = dislikes
                             continue
-                        except:
-                            y_data["dislikeCount"] = 0
-                            continue
-                    if "likes" in element.get_attribute("aria-label") or "like" in element.get_attribute("aria-label"):
-                        try:
+                    except:
+                        y_data["dislikeCount"] = 0
+                        continue
+                    try:
+                        if "likes" in element.get_attribute("aria-label") or "like" in element.get_attribute("aria-label"):
                             likes = int (element.get_attribute("aria-label").replace(",", "").split()[0])
                             y_data["likeCount"] = likes
-                        except:
-                            y_data["likeCount"] = 0
+                    except:
+                        y_data["likeCount"] = 0
                 #print(y_data)
                 v_count = chrome.find_element_by_xpath('//div[@id = "count" and @class="style-scope ytd-video-primary-info-renderer"]')
                 try:
@@ -131,7 +133,8 @@ def main():
                     count = 0
                 y_data["viewCount"] = count
                 chrome.implicitly_wait(30)
-                chrome.execute_script("window.scrollTo(0, 5000);")
+                chrome.execute_script("window.scrollTo(3000, 5000);")
+                chrome.implicitly_wait(30)
                 n_comments =  chrome.find_element_by_xpath('//yt-formatted-string[@class="count-text style-scope ytd-comments-header-renderer"]')
                 try:
                     n_comm = int(n_comments.text.replace(",", "").split()[0])
@@ -139,10 +142,30 @@ def main():
                     n_comm = 0
                 y_data["commentCount"] = n_comm
                 y_list.append(y_data)
+                
+                if i % 5 == 0:
+                    getProx()
+                    proxy_index = random_proxy()
+                    proxy = proxies[proxy_index]
+                # Make the call
+                try:
+                    my_ip = urlopen(req).read().decode('utf8')        
+                    print('#' + str(index) + ': ' + my_ip)
+                        
+                  
+                except:  
+                    # If error, delete this proxy and find another one
+                    #global proxies
+                    del proxies[proxy_index]
+                    print('proxy deleted')
+                    proxy_index = random_proxy()
+                    proxy = proxies[proxy_index]
+                    
             mylist.append({"youtube":y_list})
             file = open(str(alt_id) + ".txt", "w+")
             json.dump(mylist, file)
             file.close()
+            
         except NoSuchElementException:
             alt_id = row["Altmetric_id"]
             print('No Such - alt_id = ' + str(alt_id))
@@ -156,23 +179,3 @@ def main():
             file = open(str(alt_id) + ".txt", "w+")
             file.close()
             pass
-        
-        if index % 5 == 0:
-            getProx()
-            proxy_index = random_proxy()
-            proxy = proxies[proxy_index]
-            
-    
-        # Make the call
-        try:
-            my_ip = urlopen(req).read().decode('utf8')        
-            print('#' + str(index) + ': ' + my_ip)
-                
-          
-        except:  
-            # If error, delete this proxy and find another one
-            #global proxies
-            del proxies[proxy_index]
-            print('proxy deleted')
-            proxy_index = random_proxy()
-            proxy = proxies[proxy_index]
